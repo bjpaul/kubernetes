6b609381364df93e987ce38ce3cc6f49e6b3363d
Bijoy Paul <work.bijoypaul@gmail.com>
2019-10-18 16:32:14 +0530

    NodePort services added

--- beginner/react-app/src/env.js
+++ beginner/react-app/src/env.js
@@ -1,9 +1,19 @@
-let WRITER_PROTOCOL = process.env.WRITER_PROTOCOL || "http://"
-let WRITER_HOST = process.env.WRITER_HOST || "localhost"
-let WRITER_PORT = process.env.WRITER_PORT || 8081
+// let WRITER_PROTOCOL = process.env.WRITER_PROTOCOL || "http://"
+// let WRITER_HOST = process.env.WRITER_HOST || "localhost"
+// let WRITER_PORT = process.env.WRITER_PORT || 8081
+// module.exports.WRITER_ENDPOINT = WRITER_PROTOCOL+WRITER_HOST+':'+WRITER_PORT
+
+// let READER_PROTOCOL = process.env.READER_PROTOCOL || "http://"
+// let READER_HOST = process.env.READER_HOST || "localhost"
+// let READER_PORT = process.env.READER_PORT || 8080
+// module.exports.READER_ENDPOINT = READER_PROTOCOL+READER_HOST+':'+READER_PORT
+
+let WRITER_PROTOCOL = "http://"
+let WRITER_HOST = "54.157.106.85"
+let WRITER_PORT = "31235"
 module.exports.WRITER_ENDPOINT = WRITER_PROTOCOL+WRITER_HOST+':'+WRITER_PORT
 
-let READER_PROTOCOL = process.env.READER_PROTOCOL || "http://"
-let READER_HOST = process.env.READER_HOST || "localhost"
-let READER_PORT = process.env.READER_PORT || 8080
+let READER_PROTOCOL = "http://"
+let READER_HOST = "54.157.106.85"
+let READER_PORT = "31234"
 module.exports.READER_ENDPOINT = READER_PROTOCOL+READER_HOST+':'+READER_PORT
